4fda48dff5c9d1c9701724cb76c2cad0b22e0738
Anees shaikh <aneesshaikh329@gmail.com>
2026-03-22 10:44:45 +0530

    removed console logs

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -213,8 +213,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 const refreshAccessToken = asyncHandler(async (req, res) => {
     const incomingRefreshToken = req.cookies?.refreshToken
         || req.body.refreshToken
-    console.log("COOKIES:", req.cookies) // ← yeh add kar
-    console.log("BODY:", req.body)
+    
 
     if (!incomingRefreshToken) {
         throw new ApiError(401, "Unauthorized request")
